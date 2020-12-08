1b4afc810bd0b55284f79f51c89752a3c42d96ae
wixancl <wixancl@gmail.com>
2020-12-08 04:34:54 +0000

    2020_12_08_04_34_54

--- install.sh
+++ install.sh
@@ -1,42 +1,44 @@
+echo "Instalacion Preliminar"
+read -rsp $'Presione cualquier tecla o espere 5 segundos para continuar  \n' -n 1 -t 5;
 apt-get update
 apt-get upgrade -y
 apt-get install iputils-ping -y
 apt-get install net-tools -y
 apt-get install netstat -y
-apt-get install apg 
-apt-get install bmon 
-apt-get isntall bsdmainutils 
-apt-get install byobu 
-apt-get install ccze 
-apt-get install cmatrix 
-apt-get install fontconfig 
-apt-get install gawk 
-apt-get install gettext-base 
-apt-get install jp2a 
-apt-get install kbd 
-apt-get install keyboard-configuration 
-apt-get install speedometer 
-apt-get install tmux 
-apt-get install tree
-apt-get install vim
-apt-get install xxd
-apt-get install cpp 
-apt-get install wamerican
-apt-get install binutils
-apt-get install ctags
-apt-get install whois
-apt-get install vacation
-apt-get install nas
-apt-get install pulseaudio
-apt-get install jackd2
-apt-get install sndiod
-apt-get install speex
-apt-get install netselect
-apt-get install fping 
-apt-get install mc
-apt-get install mlocate
-apt-get install moreutils
-apt-get install mplayer
-apt-get install htop
+apt-get install apg -y
+apt-get install bmon -y
+apt-get isntall bsdmainutils -y 
+apt-get install byobu -y
+apt-get install ccze -y
+apt-get install cmatrix -y 
+apt-get install fontconfig -y 
+apt-get install gawk -y
+apt-get install gettext-base -y 
+apt-get install jp2a -y
+apt-get install kbd -y
+apt-get install keyboard-configuration -y 
+apt-get install speedometer -y
+apt-get install tmux -y
+apt-get install tree -y
+apt-get install vim -y
+apt-get install xxd -y
+apt-get install cpp -y
+apt-get install wamerican -y
+apt-get install binutils -y
+apt-get install ctags -y
+apt-get install whois -y
+apt-get install vacation -y
+apt-get install nas -y
+apt-get install pulseaudio -y
+apt-get install jackd2 -y
+apt-get install sndiod -y
+apt-get install speex -y
+apt-get install netselect -y
+apt-get install fping -y
+apt-get install mc -y
+apt-get install mlocate -y
+apt-get install moreutils -y
+apt-get install mplayer -y
+apt-get install htop -y
 apt-get install python3 -y
 apt-get install python3-pip -y
---
+++ net.sh
@@ -0,0 +1,15 @@
+echo "Instalacion Preliminar"
+read -rsp $'Presione cualquier tecla o espere 5 segundos para continuar  \n' -n 1 -t 5;
+apt-get update
+apt-get upgrade -y
+apt-get install iputils-ping -y
+apt-get install net-tools -y
+apt-get install netstat -y
+apt-get install bmon -y
+apt-get install speedometer -y
+apt-get install whois -y
+apt-get install speex -y
+apt-get install netselect -y
+apt-get install fping -y
+apt-get install nmap -y
+
