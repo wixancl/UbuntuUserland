7a5722ab8e00fc393ab1f9b23733df003d559801
wixancl <wixancl@gmail.com>
2020-12-09 16:53:22 +0000

    2020_12_09_16_53_22

--- install.sh
+++ install.sh
@@ -3,22 +3,51 @@ read -rsp $'Presione cualquier tecla o espere 5 segundos para continuar  \n' -n 
 # Actualizar Servidor
 apt-get update
 apt-get upgrade -y
+# Instalacion de utilidad de Red
+#
+apt-get install fping
+#
 apt-get install iputils-ping -y
+#
 apt-get install net-tools -y
+#
 apt-get install netstat -y
-apt-get install apg -y
+#
+apt-get install speedometer -y
+#
+apt-get install whois -y
+#
+apt-get install netselect -y
+#
 apt-get install bmon -y
-apt-get isntall bsdmainutils -y 
-apt-get install byobu -y
-apt-get install ccze -y
+#
+apt-get install nmap -y
+#
+apt-get install netselect -y
+#
+apt-get install fping -y
+#
+apt-get install arp-scan -y
+#
+
+
+
+
+
+
+apt-get install speex -y
+apt-get install apg -y
 apt-get install cmatrix -y 
+
 apt-get install fontconfig -y 
 apt-get install gawk -y
 apt-get install gettext-base -y 
 apt-get install jp2a -y
 apt-get install kbd -y
 apt-get install keyboard-configuration -y 
-apt-get install speedometer -y
+
+
+
 apt-get install tmux -y
 apt-get install tree -y
 apt-get install vim -y
@@ -27,15 +56,14 @@ apt-get install cpp -y
 apt-get install wamerican -y
 apt-get install binutils -y
 apt-get install ctags -y
-apt-get install whois -y
+
 apt-get install vacation -y
 apt-get install nas -y
 apt-get install pulseaudio -y
 apt-get install jackd2 -y
 apt-get install sndiod -y
-apt-get install speex -y
-apt-get install netselect -y
-apt-get install fping -y
+
+
 apt-get install mc -y
 apt-get install mlocate -y
 apt-get install moreutils -y
@@ -44,6 +72,6 @@ apt-get install htop -y
 #Instalacion de python y aplicativos
 apt-get install python3 -y
 apt-get install python3-pip -y
-#instalacion de reportlab para python
+#instalacion de reportlab para Python3
 
 
