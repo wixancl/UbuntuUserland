3f991f6a033433acf08283c3883188433edd5964
wixancl <wixancl@gmail.com>
2020-12-10 02:52:53 +0000

    2020_12_10_02_52_53

--- config.sh
+++ config.sh
@@ -3,3 +3,14 @@ echo "Configuracion Terminal"
 echo " Configuracion de color "
 setterm -foreground white -background black
 clear
+mkdir -p crondir
+chmod 777 crondir
+# archivo Existe ?
+if [ -f /crondir/01.sh ];
+then
+	echo "Existe archivo"
+else
+	echo "No Existe"
+	echo " " > 001.sh
+fi
+
