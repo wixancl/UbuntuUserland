e4534b5576491afa1a4fe47558cdd483a647a56a
wixancl <wixancl@gmail.com>
2020-12-13 18:29:03 +0000

    2020_12_13_18_29_03

--- py/index.py
+++ py/index.py
@@ -1,4 +1,46 @@
+# Importar dependencias para manejo del sistema operativo
 import os
+
+#importar dependencias para el manejo de fechas
+from datetime import date
+from datetime import datetime
+
+#Titulo program 
 print('s python ')
-print(' os.getlogin() ')
+
+##############################################################################################################################
+#Mostrar fecha y hora
+today = date.today()
+print(today)
+print("El dia actual es {} ".format(today.day))
+print("El mes actual es {} ".format(today.month))
+print("El agno actual es {} ".format(today.year))
+#
+##############################################################################################################################
+#
+##############################################################################################################################
+#
+#Fecha actual
+now = datetime.now()
+print(now)
+print("El dia actual es {} ".format(now.day))
+print("El mes actual es {} ".format(now.month))
+print("El agno actual es {} ".format(now.year))
+print("La hora actual es {}".format(now.hour))
+print("El hora actual es {}".format(now.minute))
+print("El segundo actual es {}".format(now.second))
+formatofecha = now.strftime('Dia :%d, Mes :%m, Agno :%Y, Hora :%H, Minutos :%M, Segundos :%S')
+print(formatofecha)
+
+##############################################################################################################################
+
+#
+
+#Crear log
+file = open("log.txt", "w")
+file.write(" 1 " + os.linesep)
+file.write(" 2 ")
+file.close()
+
+
 
