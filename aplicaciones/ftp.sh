9830a3738e57131683913cda83819fe08c838ad6
wixancl <wixancl@gmail.com>
2020-12-15 13:59:05 +0000

    2020_12_15_13_59_05

--- aplicaciones/ftp.sh
+++ aplicaciones/ftp.sh
@@ -2,21 +2,3 @@
 ##############################################################################
 #########ncftpput -f login.txt / /home/administrador/SH/py/02.txt
 #########ncftpget -f login.txt /home/administrador/SH/py/ /01.txt
-# Se crea variable de tiempo Fecha/Hora
-date_stamp=$(date +"%Y_%m_%d_%H_%M_%S")
-
-
-
-###########################################################################################################################
-#Configuracion para repositorio
-# directorio existe ?
-if [ -d /$date_stamp ];
-then
-	echo "El Directorio $date_stamp existe no se ha realizado ningun cambio"
-else
-	echo "El Directorio $date_stamp no existe. se procede a crear"
-	mkdir -p $date_stamp
-	chmod 777 $date_stamp
-fi
-
-
