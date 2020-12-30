214bcdd2e5629fd581c25a6b7cf7def5b610f1be
root <root@localhost>
2020-12-30 03:25:31 +0000

    2020_12_30_03_25_30

--- informe/informe.sh
+++ informe/informe.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 FECHA_Y_HORA=`date "+%d-%m-%y_%H-%M-%S"`
 NOMBRE_SISTEMA="Metone"
-RUTA="/var/script/storage/informe/"
+#RUTA="/var/script/storage/informe/"
+RUTA="/home/administrador/"
+
+
 EXTENSION=".txt"
 SEPARADOR=" ------------------------------------------------------------------------------------------------------------------------------ "
 CADENA=$RUTA$FECHA_Y_HORA$EXTENSION
